326a1118e42e364bb6bdf6fb676d74579e2c4e27
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-02-14 13:19:01 +0000

    test updated to take legitimate input, test passes.  Code refactored

--- src/__tests__/bandpass.test.js
+++ src/__tests__/bandpass.test.js
@@ -1,15 +1,17 @@
 import Bandpass from '../bandpass.js'
 
+beforeEach( () => {
+  test = new Bandpass
+})
+
 describe('test class exists', () => {
   test('bandpass class', () => {
-    test = new Bandpass
     expect(test).toBeInstanceOf(Bandpass)
   })
 })
 
-describe('input outputs array', () => {
-  test('bandpass class', () => {
-    test = new Bandpass
-    expect(test.filter()).toEqual([])
+describe('testing filter function', () => {
+  test('input min, max and frequences outputs correct array', () => {
+    expect(test.filter(20, 9000,[10,15,20,600,2000,10000,18000])).toEqual([20,20,20,600,2000,9000,9000])
   })
 })
--- src/bandpass.js
+++ src/bandpass.js
@@ -1,8 +1,18 @@
 export default class Bandpass {
 
-filter(){
-  return []
-}
+filter(min, max, array){
+  // replace frequencies < min with min
+  // replace frequencies > max with max
+  // output new array
+  let filteredFreqs = []
 
+array.forEach(item => {
+  if(item < min) filteredFreqs.push(min)
+  if(item > max) filteredFreqs.push(max)
+  if(item >= min && item <= max) filteredFreqs.push(item)
+})
+
+  return filteredFreqs
+}
 
 }
